a29fb3e5214c0c3b6b837ad50402c409322a695f
Wanderson Oliveira Silva <wanderson1873@live.com>
2020-08-05 04:06:36 +0100

    up

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import backIcon from '../../assets/images/icons/back.svg'
+import logoImg from '../../assets/images/logo.svg'
+
+import './styles.css';
 
 function TeacherList() {
     return (
-        <h1>Teachers</h1>
+        <div id="page-teacher-list" className="container">
+            <header className="page-header">
+                <div className="top-bar-container">
+                    <Link to="/">
+                        <img src={backIcon} alt="Voltar"/>
+                    </Link>
+                    <img src={logoImg} alt="Proffy"/>
+                </div>
+
+                <div className="header-content">
+                    <strong>Estes são os proffys disponiveis.</strong>
+                </div>
+            </header>
+        </div>
     )
 }
 
