e84183bee9a178d195478488888949a292fdd65d
Wanderson Oliveira Silva <wanderson1873@live.com>
2020-08-05 17:34:02 +0100

    main

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
-import './styles.css';
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import './styles.css';
 
 function TeacherList() {
     return (
@@ -27,6 +28,35 @@ function TeacherList() {
                     </div>
                 </form>
             </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars2.githubusercontent.com/u/65118827?s=460&u=7c508a738d926eb0f20fdef0502bd2aab874f51c&v=4" alt="Wanderson O" />
+                        <div>
+                            <strong>Wanderson Oliveira</strong>
+                            <span>Quimica</span>
+                        </div>
+                    </header>
+
+                    <p>
+                        Entusiasta das melhores tecnologias de química avançada
+                       <br /><br />
+                       Apaixonado por explodir coisas em labotatório e por mudar a vida de pessoas com suas explosôes.
+                    </p>
+
+                    <footer>
+                        <p>
+                            Preço/Hora
+                            <strong>R$ 80,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={whatsappIcon} alt="whatsapp" />
+                                Entrar em contato
+                            </button>
+                    </footer>
+                </article>
+            </main>
         </div>
     );
 }
