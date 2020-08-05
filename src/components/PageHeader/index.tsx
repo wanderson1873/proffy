e32effe402540b072e2451f33448aa673dd664a2
Wanderson Oliveira Silva <wanderson1873@live.com>
2020-08-05 16:41:04 +0100

    page header

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 import backIcon from '../../assets/images/icons/back.svg'
 import logoImg from '../../assets/images/logo.svg'
 
+import './styles.css';
+
 interface PageHeaderProps {
     title: string;
 }
