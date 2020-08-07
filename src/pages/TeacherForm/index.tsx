f1c380d173a999b0f0bed9609cd0e8015ff78859
Wanderson Oliveira Silva <wanderson1873@live.com>
2020-08-07 03:43:59 +0100

    styles

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/input';
+
+import warningIcon from '../../assets/images/icons/warning.svg'
 
 import './styles.css'
 
@@ -15,22 +18,27 @@ function TeacherForm() {
             <main>
                 <fieldset>
                     <legend>Seus dados</legend>
-                    
-                    <div className="input-block">
-                        <label htmlFor="name">Nome completo</label>
-                        <input type="text" id="name" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="avatar">Avatar</label>
-                        <input type="text" id="avatar" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="whatsapp">WhatsApp</label>
-                        <input type="text" id="whatsapp" />
-                    </div>
+                    <Input name="name" label="Nome completo" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="WhatsApp" />
                 </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+                    <Input name="subject" label="Matéria" />
+                    <Input name="cost" label="Custo da sua hora por aula" />
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="aviso" />
+                        Importante! <br />
+                        Preencha todos os dados
+                    </p>
+                    <button type="button">
+                        Salvar cadastro
+                    </button>
+                </footer>
             </main>
         </div>
     )
