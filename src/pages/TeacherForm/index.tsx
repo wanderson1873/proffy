da1d1ab1e42f145f30b4895ed3e4e1fb76680377
Wanderson Oliveira Silva <wanderson1873@live.com>
2020-08-07 02:57:52 +0100

    input

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -2,12 +2,38 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import './styles.css'
+
 function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container">
-            <PageHeader title="Que incivel que você quer da aulas." />
+            <PageHeader 
+                title="Que incivel que você quer da aulas." 
+                description="O primeiro passo é preencher esse formulário."
+            />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+                    
+                    <div className="input-block">
+                        <label htmlFor="name">Nome completo</label>
+                        <input type="text" id="name" />
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="avatar">Avatar</label>
+                        <input type="text" id="avatar" />
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="whatsapp">WhatsApp</label>
+                        <input type="text" id="whatsapp" />
+                    </div>
+                </fieldset>
+            </main>
         </div>
-    );
+    )
 }
 
 export default TeacherForm;
