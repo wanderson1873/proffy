1b93d5a4bda059d3328000c1207ce7f313f8d217
Wanderson Oliveira Silva <wanderson1873@live.com>
2020-08-08 17:21:16 +0100

    Update index.tsx

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 
 import LogoImg from '../../assets/images/logo.svg';
@@ -8,9 +8,22 @@ import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
+import api from '../../service/api';
+
+
 import './styles.css';
 
 function Landing() {
+    const [totalConnections, setTotalConnections] = useState(0)
+
+    useEffect(() => {
+        api.get('/connections').then(response => {
+            const { total } = response.data
+
+            setTotalConnections(total)
+        })
+    }, [])    
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -37,7 +50,7 @@ function Landing() {
                     </Link>
                 </div>
                 <span className="total-connections">
-                    Total de 200 conexôes já realizadas <img src={purpleHeartIcon} alt="coração"/>
+                    Total de {totalConnections} conexôes já realizadas <img src={purpleHeartIcon} alt="coração"/>
                 </span>
             </div>
         </div>
